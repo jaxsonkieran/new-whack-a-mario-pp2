f557c416fe1b8c6c8b237ea4e23ca030a5fddb83
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-20 17:36:50 +0000

    add comments for functions needed to be created to js file

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// create function that counts down time
+// create function that holds the score and adds to each time a mario is hit
+// create function that produces the mario in a random hole
+
+
+// declare game variables
