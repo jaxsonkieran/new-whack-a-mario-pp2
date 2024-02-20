5cda65d6269d072edbfc7c75f5789656a6dd6d6c
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-20 17:47:55 +0000

    create a function for the mario to appear in random holes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,21 @@ const holes = document.querySelectorAll('.hole');
 const mario = document.querySelector('.mario');
 const score = document.querySelector('#score');
 const timeLeft = document.querySelector('#time-left');
+
+
+
+/**
+ * function to get the mario to appear in random holes on the container
+ */
+
+function randomHole () {
+    // remove the mario class on initial holes so the mario appears on a fresh hole each time
+    holes.forEach(hole => {
+        hole.classList.remove('mario');
+    })
+    // get a random number from 0 - 8 (1-9), as we start counting from 0, for the mario to appear in
+    let randomHole = holes[Math.floor(Math.random() * 9)];
+    randomHole.classList('mario');
+}
+
+randomHole();
